3e544f827781b4288972e6f24d37918ee22771c1
David Lee <Dai180293@gmail.com>
2023-07-18 10:02:11 +0100

    added fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,9 @@ const App = () => {
     return <>hello</>;
 };
 
+// var fullList = [1,2,3,4],
+//     array of selected tags = [2,4],
+//     filteredLists = arr1.filter(item => arr2.includes(item));
+// console.log(res);
+
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                LeagueSpartan: ["League Spartan", "sans-serif"],
+            },
+            colors: {
+                primary: {
+                    DarkCyan: "hsl(180, 29%, 50%)",
+                },
+                neutral: {
+                    background: "hsl(180, 52%, 96%)",
+                    pillBG: "hsl(180, 31%, 95%)",
+                    darkGrayCyan: "hsl(180, 8%, 52%)",
+                    darkerGrayCyan: "hsl(180, 14%, 20%)",
+                },
+            },
+        },
     },
     plugins: [],
 };
