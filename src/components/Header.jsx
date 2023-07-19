3a52d415eaab11186e7493a3e5bb51ea7f6dd5b4
David Lee <Dai180293@gmail.com>
2023-07-20 00:05:58 +0100

    revert back to original file structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import desktop from "../../images/bg-header-desktop.svg";
+import desktop from "../images/bg-header-desktop.svg";
 const Header = () => {
     return (
         <header className="bg-primary-DarkCyan h-28 md:h-auto">
-            <img src={desktop} alt="banner" className="h-full"/>
+            <img src={desktop} alt="banner" className="h-full" />
         </header>
     );
 };
